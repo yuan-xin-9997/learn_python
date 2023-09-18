487de26c94a13776e2a36eb16d6230b61d63daaf
yuan.xin <yuanxin9997@qq.com>
2023-09-18 21:15:32 +0800

    2023-09-18 03

--- async_programming/7_task_demo_1.py
+++ async_programming/7_task_demo_1.py
@@ -1,9 +1,12 @@
 import asyncio
+import time
 
 
 async def func(n):
     print(n)
     await asyncio.sleep(2)
+    # asyncio.sleep(2)
+    # time.sleep(2)
     print(n+1)
     return "返回值"
 
@@ -22,7 +25,9 @@ async def main():
     # 当执行某协程遇到IO操作时，会自动化切换执行其他任务。
     # 此处的await是等待相对应的协程全都执行完毕并获取结果
     ret1 = await task1
+    # ret1 = task1
     ret2 = await task2
+    # ret2 = task2
     print(ret1, ret2)
 
 
